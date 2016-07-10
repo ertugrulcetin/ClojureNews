ee05bee326fa40b9c0c13d3adb0834b1feac0710
ertugrulcetin <ertugrul.cetin@mebitech.com>
2016-07-10 12:39:14 +0300

    Readme.md update

---
+++ src/clj/dao/db_conf.clj
@@ -0,0 +1,14 @@
+(ns clj.dao.db-conf
+  (:require [monger.core :as mg]
+            [monger.credentials :as mcr]))
+
+(def username "my-username")
+(def database-name "my-database-name")
+(def database-password "my-password")
+(def host "localhost")
+
+(defonce creds (mcr/create username database-name database-password))
+
+(defonce conn (mg/connect-with-credentials host creds))
+
+(defonce clojure-news (mg/get-db conn database-name))
